9ea50b176a91ede942998b10dd01da6ea09aa061
kior <jooozip@yonsei.ac.kr>
2025-08-25 00:28:00 +0900

    upd. release 20250825-0027

--- 8.5-REAL-webadmin-compose-build/build/build.sh
+++ 8.5-REAL-webadmin-compose-build/build/build.sh
@@ -1,2 +1,3 @@
 # internal-registry Build Command
-DOCKER_BUILDKIT=0 docker build --platform linux/amd64 --no-cache --force-rm -t tmaxsoft-jeus:v8.5 -f ./DockerFile.jeus ../
+docker build --platform linux/amd64 --no-cache --force-rm -t tmaxsoft-jeus:v8.5 -f ./DockerFile.jeus ../
+#podman build --platform linux/amd64 --no-cache --force-rm -t tmaxsoft-jeus:v8.5 -f ./DockerFile.jeus ../
